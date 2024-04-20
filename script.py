3ec39ee615ac1bd77138bf634d9fe3b81beb02aa
cipher <asaforbrian18@gmail.com>
2024-04-20 15:46:27 +0300

    navigating to fee calculator tab

--- script.py
+++ script.py
@@ -1,43 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 import time
+
+
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
+
 driver.get("https://www.emu.edu.tr/en")
+
 
 academics_element = driver.find_element(By.XPATH, "//*[@id='mgmenu1']/ul/li[3]/a")
 academics_element.click()
-
 time.sleep(2)
 
 card_element = driver.find_element(By.XPATH, "//*[@id='contentskip']/div[4]/div/div/div[2]/a")
 card_element.click()
-
-
 time.sleep(2)
 
 department_name_element =  driver.find_element(By.XPATH, "//*[@id='contentskip']/div[1]/div/div/table/tbody/tr[18]/td[1]/a")
 department_name_element.click()
-
 time.sleep(2)
 
-curriculum_element = driver.find_element(By.XPATH, "//*[@id='sticky_nav']/li[2]/a")
-curriculum_element.click()
+fee_calculator_element = driver.find_element(By.XPATH, "//*[@id='sticky_nav']/li[4]/a")
+fee_calculator_element.click()
 
 
 
-
-
-
-
-
-time.sleep(10)
-
+time.sleep(5)  
+# Quit the driver
 driver.quit()
